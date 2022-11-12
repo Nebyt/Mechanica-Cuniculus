58dc11e67606ea29cb80317b9bb48765b922457c
Denis <nebytov.d@gmail.com>
2022-11-12 20:51:05 +0300

    using one field instead on 15

--- src/components/TopPart.js
+++ src/components/TopPart.js
@@ -1,8 +1,20 @@
 import React, {useState} from 'react';
 import InputField from './InputField';
+import {validateMnemonic, mnemonicToEntropy} from 'bip39';
 
 const TopPart = () => {
   const [seedPhraseValue, setSeedPhraseValue] = useState('');
+
+  const checkSeedPhrase = () => {
+    // please mail whisper fork family fee east butter property cover car soap immense divide forget
+    console.log(`The seed phrase is: ${JSON.stringify(seedPhraseValue)}`);
+    // the error Uncaught ReferenceError: Buffer is not defined is here
+    const entropy = mnemonicToEntropy(seedPhraseValue);
+    console.log(`The seed phrase entropy is: ${JSON.stringify(entropy)}`);
+    // it returns false
+    const checkResult = validateMnemonic(`${seedPhraseValue}`);
+    console.log(`The seed phrase is valid: ${checkResult}`);
+  };
 
   return (
     <>
@@ -12,6 +24,15 @@ const TopPart = () => {
             inputLabel="Seed phrase"
             currenFieldValue={seedPhraseValue}
             onChange={(event) => { setSeedPhraseValue(event.target.value) }}/>
+          <div className="grid justify-items-center">
+            <button
+              type="button"
+              className="text-white font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center bg-green-800 hover:bg-green-700 focus:ring-green-600"
+              onClick={checkSeedPhrase}
+            >
+              Check
+            </button>
+          </div>
         </div>
       </div>
     </>
