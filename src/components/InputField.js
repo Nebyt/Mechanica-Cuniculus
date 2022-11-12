ce921f77f4c03a4d1592baa204077937105cf53b
Denis <nebytov.d@gmail.com>
2022-11-12 10:47:40 +0300

    using one field instead on 15

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
-const InputField = ({inputIndex, currenFieldValue, onChange}) => {
+const InputField = ({inputLabel, currenFieldValue, onChange}) => {
   return (
-    <div className="mb-6 pr-4" id={`inputField-${inputIndex}`}>
+    <div className="mb-6 pr-4" id={`inputField-${inputLabel.toLowerCase().replace(/ /g, '_')}`}>
       <label
         htmlFor="NFTName"
-        className="block text-sm font-medium text-gray-300"
-        id={`inputLabel-${inputIndex}`}
+        className="block mb-1 text-sm font-medium text-gray-300"
+        id={`inputLabel-${inputLabel.toLowerCase().replace(/ /g, '_')}`}
       >
-        {inputIndex+1}.
+        {inputLabel}:
       </label>
       <input
         type="text"
-        id={`input-${inputIndex}`}
+        id={`input-${inputLabel.toLowerCase().replace(/ /g, '_')}`}
         className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
-        value={currenFieldValue()}
+        value={currenFieldValue}
         onChange={onChange}
       />
     </div>
--- src/components/TopPart.js
+++ src/components/TopPart.js
@@ -2,54 +2,16 @@ import React, {useState} from 'react';
 import InputField from './InputField';
 
 const TopPart = () => {
-  const [currentFieldValue, setFieldValue] = useState([
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-  ]);
-
-  const changeFieldValue = (valueIndex, event) => {
-    currentFieldValue[valueIndex] = event.target.value;
-    setFieldValue(currentFieldValue);
-    console.log(JSON.stringify(currentFieldValue));
-  };
-
-  const getFieldValue = (valueIndex) => {
-    return currentFieldValue[valueIndex];
-  }
-
-  const getInputFields = () => {
-    const resultArray = [];
-    for (let i = 0; i < 15; i++) {
-        resultArray.push(
-          <InputField
-            inputIndex={i}
-            currenFieldValue={(valueIndex) => getFieldValue(valueIndex)}
-            onChange={(ev) => changeFieldValue(i, ev)}/>
-        );
-    }
-    return resultArray;
-  };
+  const [seedPhraseValue, setSeedPhraseValue] = useState('');
 
   return (
     <>
       <div className="grid justify-items-center py-5 px-5" key="topPartGrid">
         <div className="block p-6 min-w-full rounded-lg border shadow-md bg-gray-800 border-gray-700" key="topPartBlock">
-          <div className="grid grid-cols-5 p-6" key="topPartInputs">
-            {getInputFields()}
-          </div>
+          <InputField
+            inputLabel="Seed phrase"
+            currenFieldValue={seedPhraseValue}
+            onChange={(event) => { setSeedPhraseValue(event.target.value) }}/>
         </div>
       </div>
     </>
